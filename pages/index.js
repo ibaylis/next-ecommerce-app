89fe5541181b47c7b8ed6bf1f30c58f7df8033ce
Ian Baylis <ianbaylis@USMDLT824.fios-router.home>
2021-10-11 22:09:55 -0400

    Fixing linting issues

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Ian's Store
+          The Baylis Code&apos;s Store
         </h1>
 
         <p className={styles.description}>
